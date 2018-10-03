d6723c7226b966a24feb0c47dcaefb3ac7c30942
Bruno Lopes <bruno.lopes.ti@icloud.com>
2018-10-03 18:32:59 -0300

    Criação do projeto de recomendação

---
+++ deep_learning.py
@@ -0,0 +1,201 @@
+import numpy as np
+import pandas as pd
+
+
+def relu(Z):
+    A = np.maximum(0, Z)
+
+    assert (A.shape == Z.shape)
+
+    cache = Z
+    return A, cache
+
+
+def relu_backward(dA, cache):
+    Z = cache
+    dZ = np.array(dA, copy=True)  # just converting dz to a correct object.
+
+    # When z <= 0, you should set dz to 0 as well.
+    dZ[Z <= 0] = 0
+
+    assert (dZ.shape == Z.shape)
+
+    return dZ
+
+
+def inicializa_parametros_deep(layer_dims):
+
+    np.random.seed(1)
+    parameters = {}
+    L = len(layer_dims)  # number of layers in the network
+
+    for l in range(1, L):
+        parameters['W' + str(l)] = np.random.randn(layer_dims[l], layer_dims[l - 1]) / np.sqrt(
+            layer_dims[l - 1])  # *0.01
+        parameters['b' + str(l)] = np.zeros((layer_dims[l], 1))
+
+        assert (parameters['W' + str(l)].shape == (layer_dims[l], layer_dims[l - 1]))
+        assert (parameters['b' + str(l)].shape == (layer_dims[l], 1))
+
+    return parameters
+
+
+def Tamanho_Camadas(X):
+    n_x = X.shape[1]
+    n_h = 2
+    n_y = X.shape[1]
+
+    return (n_x, n_h, n_y)
+
+
+def para_frente_linear(A, W, b):
+    Z = np.dot(W, A) + b
+
+    assert (Z.shape == (W.shape[0], A.shape[1]))
+    cache = (A, W, b)
+
+    return Z, cache
+
+
+def para_frente_linear_ativacao(A_prev, W, b):
+
+    Z, linear_cache = para_frente_linear(A_prev, W, b)
+    A, activation_cache = relu(Z)
+
+    assert (A.shape == (W.shape[0], A_prev.shape[1]))
+    cache = (linear_cache, activation_cache)
+
+    return A, cache
+
+
+def modelo_para_frente_L(X, parameters):
+
+    caches = []
+    A = X
+    L = len(parameters) // 2  # number of layers in the neural network
+
+    for l in range(1, L):
+        A_prev = A
+        A, cache = para_frente_linear_ativacao(A_prev, parameters['W' + str(l)], parameters['b' + str(l)])
+        caches.append(cache)
+
+    # Implement LINEAR -> SIGMOID. Add "cache" to the "caches" list.
+    AL, cache = para_frente_linear_ativacao(A, parameters['W' + str(L)], parameters['b' + str(L)])
+    caches.append(cache)
+
+    return AL, caches
+
+
+def compute_custo(AL, Y):
+
+    m = Y.shape[1]
+
+    # Compute loss from aL and y.
+    cost = (1. / m) * (-np.dot(Y, np.log(AL).T) - np.dot(1 - Y, np.log(1 - AL).T))
+
+    cost = np.squeeze(cost)  # To make sure your cost's shape is what we expect (e.g. this turns [[17]] into 17).
+    assert (cost.shape == ())
+
+    return cost
+
+
+def para_tras_linear(dZ, cache):
+    A_prev, W, b = cache
+    m = A_prev.shape[1]
+
+    dW = (1 / m) * np.dot(dZ, A_prev.T)
+    db = 1. / m * np.sum(dZ, axis=1, keepdims=True)
+    dA = np.dot(W.T, dZ)
+
+    assert (dA.shape == A_prev.shape)
+    assert (dW.shape == W.shape)
+    assert (db.shape == b.shape)
+
+    return dA, dW, db
+
+
+def para_tras_linear_ativacao(dA, cache, activation):
+    linear_cache, activation_cache = cache
+
+    if activation == "relu":
+        dZ = relu_backward(dA, activation_cache)
+        dA_prev, dW, db = para_tras_linear(dZ, linear_cache)
+
+    return dA_prev, dW, db
+
+
+def modelo_para_tras_L(AL, Y, caches):
+    grads = {}
+    L = len(caches)
+    m = AL.shape[1]
+
+    dAL = - (np.divide(Y, AL) - np.divide(1 - Y, 1 - AL))
+
+    grads["dA" + str(L)], grads["dW" + str(L)], grads["db" + str(L)] = para_tras_linear_ativacao(dAL, caches[L - 1],
+                                                                                                 activation="relu")
+    for l in reversed(range(L - 1)):
+        dA_prev, dW, db = para_tras_linear_ativacao(grads["dA" + str(l + 2)], caches[l], activation="relu")
+        grads["dA" + str(l + 1)] = dA_prev
+        grads["dW" + str(l + 1)] = dW
+        grads["db" + str(l + 1)] = db
+
+    return grads
+
+
+def atualizacao_parametros(parameters, grads, learning_rate):
+
+    L = len(parameters) // 2  # number of layers in the neural network
+
+    for l in range(L):
+        parameters["W" + str(l + 1)] = parameters["W" + str(l + 1)] - learning_rate * grads["dW" + str(l + 1)]
+        parameters["b" + str(l + 1)] = parameters["b" + str(l + 1)] - learning_rate * grads["db" + str(l + 1)]
+
+    return parameters
+
+
+def modelo_L_camadas(X, Y, layers_dims, learning_rate=0.0075, num_iterations=3000, print_cost=False):  # lr was 0.009
+
+    np.random.seed(1)
+    costs = []
+
+    parameters = inicializa_parametros_deep(layers_dims)
+
+    for i in range(0, num_iterations):
+
+        AL, caches = modelo_para_frente_L(X, parameters)
+
+        cost = compute_custo(AL, Y)
+
+        grads = modelo_para_tras_L(AL, Y, caches)
+
+        atualizacao_parametros(parameters, grads, learning_rate)
+
+        if print_cost and i % 100 == 0:
+            print("Cost after iteration %i: %f" % (i, cost))
+        if print_cost and i % 100 == 0:
+            costs.append(cost)
+
+    print('interacoes (* 10)')
+    print("Taxa de aprendizado =" + str(learning_rate))
+
+    return parameters
+
+
+
+dataset = pd.read_csv('dataset.csv', low_memory=False,
+                      usecols=['year', 'vote_count', 'vote_average', 'popularity', 'Science Fiction', 'Action', 'War',
+                               'Adventure', 'Horror', 'Thriller', 'History', 'Documentary', 'Romance',
+                               'Music', 'Drama', 'Animation', 'Mystery', 'Western', 'TV Movie', 'Fantasy', 'Comedy',
+                               'Crime', 'Family'])
+X_assess = dataset
+Y_assess = np.ones((1, X_assess.shape[1]))
+layers_dims = [X_assess.shape[1],X_assess.shape[1]]
+parameters = modelo_L_camadas(X_assess, Y_assess, layers_dims=layers_dims, num_iterations=100, print_cost=True)
+
+
+prob, caches = modelo_para_frente_L(X_assess[:1], parameters)
+
+print("W1 = " + str(parameters["W1"]))
+print("b1 = " + str(parameters["b1"]))
+print("W2 = " + str(parameters["W2"]))
+print("b2 = " + str(parameters["b2"]))
---
+++ tratamento_dados.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import numpy as np
+from ast import literal_eval
+
+
+md = pd.read_csv('dataset/movies_metadata.csv', low_memory=False)
+md['genres'] = md['genres'].fillna('[]').apply(literal_eval).apply(lambda x: [i['name'] for i in x] if isinstance(x, list) else [])
+vote_counts = md[md['vote_count'].notnull()]['vote_count'].astype('int')
+vote_averages = md[md['vote_average'].notnull()]['vote_average'].astype('int')
+C = vote_averages.mean()
+
+m = vote_counts.quantile(0.95)
+md['year'] = pd.to_datetime(md['release_date'], errors='coerce').apply(lambda x: str(x).split('-')[0] if x != np.nan else np.nan)
+
+
+qualified = md[(md['vote_count'] >= m) & (md['vote_count'].notnull()) & (md['vote_average'].notnull())][['title', 'year', 'vote_count', 'vote_average', 'popularity', 'genres']]
+qualified['vote_count'] = qualified['vote_count'].astype('int')
+qualified['vote_average'] = qualified['vote_average'].astype('int')
+
+df = pd.Series(qualified['genres']).apply(frozenset).to_frame(name='genre')
+for genre in frozenset.union(*df.genre):
+    df[genre] = df.apply(lambda _: int(genre in _.genre), axis=1)
+
+
+dataset = pd.concat([qualified, df.iloc[:, 1:]], axis= 1)
+dataset.to_csv('dataset.csv')
